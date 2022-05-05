c4dea27dd87abd0aa0bb2313bcc2d57e446b0e30
Mohammad Sobhy <31019449+MohamadSobhy@users.noreply.github.com>
2022-05-05 23:28:49 +0200

    api endpoint constants added.

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,10 @@
+class ApiEndpoints {
+  //! Private members
+  static const String _baseUrl =
+      'https://us-central1-open-images-dataset.cloudfunctions.net/';
+
+  static const String _cocoDatasetEndpoint = 'coco-dataset-bigquery';
+
+  //! Public members
+  static const String cocoDatasetEndpointUrl = _baseUrl + _cocoDatasetEndpoint;
+}
