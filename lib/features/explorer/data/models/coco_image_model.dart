9e9887a7856d671f175e6ac103c25859955fc73e
Mohammad Sobhy <31019449+MohamadSobhy@users.noreply.github.com>
2022-05-05 23:27:48 +0200

    Data layer (Entities - Repository - Usecases) completed for images search feature.

---
+++ lib/features/explorer/data/models/coco_image_model.dart
@@ -0,0 +1,50 @@
+import 'dart:convert';
+
+import '../../domain/entities/coco_image.dart';
+
+class CocoImageModel extends CocoImage {
+  const CocoImageModel({
+    required int id,
+    required String cocoUrl,
+    required String flickrUrl,
+    required List<String> captions,
+    required List<ImageSegmentationModel> segmentations,
+  }) : super(
+          id: id,
+          cocoUrl: cocoUrl,
+          flickrUrl: flickrUrl,
+          captions: captions,
+          segmentations: segmentations,
+        );
+
+  factory CocoImageModel.fromJson(
+    Map<String, dynamic> imagesJson,
+    List<ImageSegmentationModel> segmentations,
+    List<String> captions,
+  ) {
+    return CocoImageModel(
+      id: imagesJson['id'],
+      cocoUrl: imagesJson['coco_url'],
+      flickrUrl: imagesJson['flickr_url'],
+      captions: captions,
+      segmentations: segmentations,
+    );
+  }
+}
+
+class ImageSegmentationModel extends ImageSegmentation {
+  const ImageSegmentationModel({
+    required int categoryId,
+    required List<num> points,
+  }) : super(categoryId: categoryId, points: points);
+
+  factory ImageSegmentationModel.fromJson(Map<String, dynamic> parsedJson) {
+    return ImageSegmentationModel(
+      categoryId: parsedJson['category_id'],
+      points: List<num>.from(
+        (json.decode(parsedJson['segmentation'] ?? "[[]]") as List<dynamic>)
+            .first,
+      ),
+    );
+  }
+}
